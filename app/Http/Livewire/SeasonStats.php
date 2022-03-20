8efd39ffe3d8206eefe5f8ae8eea87b239fc84de
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2022-03-20 10:07:13 -0500

    Count tournament games in stats

--- app/Http/Livewire/SeasonStats.php
+++ app/Http/Livewire/SeasonStats.php
@@ -12,21 +12,31 @@ class SeasonStats extends Component
     public $totalGames;
     public $totalGamesPlayed;
     public $totalUpcomingGames;
+    public $allTourGamesArray;
+    public $allTourGamesFlat;
     public $tourGames;
     public $totalTourGames;
+    public $totalUpcomingTourGames;
+    public $totalPastTourGames;
 
     public function mount()
     {
-        $this->tourGames = Tournament::pluck('games')->toArray();
-        foreach($this->tourGames as $game) {
-            foreach ($game as $value) {
-                $singleArray[] = $value;
-            }
-        }
+//        $this->tourGames = Tournament::pluck('games')->toArray();
+//        foreach($this->tourGames as $game) {
+//            foreach ($game as $value) {
+//                $singleArray[] = $value;
+//            }
+//        }
+
+        $this->allTourGamesArray = Tournament::pluck('games')->toArray();
+        $this->allTourGamesFlat = collect($this->allTourGamesArray)->flatten(1);
+        $this->totalTourGames = $this->allTourGamesFlat->count();
+        $this->totalUpcomingTourGames = $this->allTourGamesFlat->where('date', '>=', Carbon::now()->toDateString())->count();
+        $this->totalPastTourGames = $this->allTourGamesFlat->where('date', '<', Carbon::now()->toDateString())->count();
 
         $this->totalGames = Game::count() + $this->totalTourGames;
-        $this->totalGamesPlayed = Game::where('date', '<', Carbon::now())->count() + $this->totalTourGames;
-        $this->totalUpcomingGames = Game::where('date', '>=', Carbon::now())->count();
+        $this->totalGamesPlayed = Game::where('date', '<', Carbon::now())->count() + $this->totalPastTourGames;
+        $this->totalUpcomingGames = Game::where('date', '>=', Carbon::now())->count() + $this->totalUpcomingTourGames;
     }
 
     public function render()
