f176c249696282d6c967c576ec2a0b4efbe65188
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2022-03-20 15:36:09 -0500

    Changed past game loop

--- resources/views/livewire/past-game-list.blade.php
+++ resources/views/livewire/past-game-list.blade.php
@@ -11,7 +11,7 @@
                     </tr>
                     </thead>
                     <tbody class="bg-white divide-y divide-gray-200">
-                    @foreach($games as $game)
+                    @forelse($games as $game)
                     <tr>
                         <td class="px-6 py-4 whitespace-nowrap">
                             <div class="flex items-center">
@@ -35,7 +35,9 @@
                             @endif
                         </td>
                     </tr>
-                    @endforeach
+                    @empty
+                        <li>No Upcoming Games</li>
+                    @endforelse
                     </tbody>
                 </table>
             </div>
