fd19fcc2f3c62e885d893fa9b4a6b2a6b2d12b8e
joelohrdev <55007850+joelohrdev@users.noreply.github.com>
2022-03-20 09:19:20 -0500

    Swapped checkmark for win tag

--- resources/views/livewire/tournament-list.blade.php
+++ resources/views/livewire/tournament-list.blade.php
@@ -12,9 +12,7 @@
                         <div>
                             <p class="mt-2 flex items-center text-sm text-gray-500">
                                 @if($game['outcome'] === 'win')
-                                <svg class="flex-shrink-0 mr-1.5 h-5 w-5 text-green-400" x-description="Heroicon name: solid/check-circle" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
-                                    <path fill-rule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.707-9.293a1 1 0 00-1.414-1.414L9 10.586 7.707 9.293a1 1 0 00-1.414 1.414l2 2a1 1 0 001.414 0l4-4z" clip-rule="evenodd"></path>
-                                </svg>
+                                    <span class="px-2 inline-flex text-xs leading-5 font-semibold rounded-full bg-green-100 text-green-800 mr-3"> win </span>
                                 @endif
                                 @if($game['team_score'])
                                 {{ $game['team_score'] }} - {{ $game['opponent_score'] }}
